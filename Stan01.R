708413cd364a57ce31fb7ef73f3d305cb96686c6
vidbo <David.Boughton@noaa.gov>
2015-11-24 10:14:38 -0800

    Add unique id numbers for units

--- Stan01.R
+++ Stan01.R
@@ -30,33 +30,13 @@ parseUnit_ID <- function(dat) {
   rem  <- substr(obj, start=a+1, stop=15)
   a <- regexpr("_", rem)
   unit <- as.integer(substr(rem, start=a+1, stop=15))
-  out <- data.frame(dat, site=as.character(site), unit=unit, stringsAsFactors=FALSE)
+  out <- data.frame(dat, site=as.character(site), unith=unit, stringsAsFactors=FALSE)
 }
 
 mapDat <- parseUnit_ID(mapDat)
 diveDat <- parseUnit_ID(diveDat)
 EFDat <- parseUnit_ID(EFDat)
 LWtDat <- parseUnit_ID(LWtDat)
-
-u <- mapDat$site
-v <- rep(1, length(mapDat$site))
-u <- c(u, diveDat$site)
-v <- c(v, rep(2, length(diveDat$site)))
-u <- c(u, EFDat$site)
-v <- c(v, rep(3, length(EFDat$site)))
-u <- c(u, LWtDat$site)
-v <- c(v, rep(4, length(LWtDat$site)))
-u <- factor(u)
-
-mapDat$sitef <- u[v==1]
-diveDat$sitef <- u[v==2]
-EFDat$sitef <- u[v==3]
-LWtDat$sitef <- u[v==4]
-
-mapDat$siteN <- unclass(mapDat$sitef)
-diveDat$siteN <- unclass(diveDat$sitef)
-EFDat$siteN <- unclass(EFDat$sitef)
-LWtDat$siteN <- unclass(LWtDat$sitef)
 
 # some fields that are characters should be integers
 diveDat$Dive_Pass <- as.integer(as.character(diveDat$Dive_Pass))
@@ -71,7 +51,57 @@ diveDat <- diveDat[idx,]
 idx <- ! (is.na(EFDat$EF_Pass) | is.na(EFDat$Fry) | is.na(EFDat$Juv))
 EFDat <- EFDat[idx,]
 
+# make a factor with common coding for all datasets
+u <- mapDat$site
+w <- mapDat$unith
+v <- rep(1, length(mapDat$site))
+u <- c(u, diveDat$site)
+w <- c(w, diveDat$unith)
+v <- c(v, rep(2, length(diveDat$site)))
+u <- c(u, EFDat$site)
+w <- c(w, EFDat$unith)
+v <- c(v, rep(3, length(EFDat$site)))
+u <- c(u, LWtDat$site)
+w <- c(w, LWtDat$unith)
+v <- c(v, rep(4, length(LWtDat$site)))
+u <- factor(u)
 
+# now cut the factor back up to individual datasets
+mapDat$sitef <- u[v==1]
+diveDat$sitef <- u[v==2]
+EFDat$sitef <- u[v==3]
+LWtDat$sitef <- u[v==4]
+
+mapDat$siteN <- unclass(mapDat$sitef)
+diveDat$siteN <- unclass(diveDat$sitef)
+EFDat$siteN <- unclass(EFDat$sitef)
+LWtDat$siteN <- unclass(LWtDat$sitef)
+
+
+# create unique id numbers for each unit (may need to include year as well if units differ year to year)
+units <- unique(data.frame(siteN=unclass(u), unith=w))
+ord <- order(units$siteN, units$unith)
+units <- units[ord,]
+units$unit <- 1:dim(units)[1]  # new unique id number for units
+
+# assign unique unit ids to datasets
+idx <- match(paste0(as.character(mapDat$siteN), "_", as.character(mapDat$unith)) , 
+             paste0(as.character(units$siteN),  "_", as.character(units$unith)))
+mapDat$unit <- units$unit[idx]
+
+idx <- match(paste0(as.character(diveDat$siteN), "_", as.character(diveDat$unith)) , 
+             paste0(as.character(units$siteN),   "_", as.character(units$unith)))
+diveDat$unit <- units$unit[idx]
+
+idx <- match(paste0(as.character(EFDat$siteN), "_", as.character(EFDat$unith)) , 
+             paste0(as.character(units$siteN), "_", as.character(units$unith)))
+EFDat$unit <- units$unit[idx]
+
+idx <- match(paste0(as.character(LWtDat$siteN), "_", as.character(LWtDat$unith)) , 
+             paste0(as.character(units$siteN), "_", as.character(units$unith)))
+LWtDat$unit <- units$unit[idx]
+
+# save the final datasets
 save(mapDat, diveDat, EFDat, LWtDat, file="VenturaData.RData")
 
 
